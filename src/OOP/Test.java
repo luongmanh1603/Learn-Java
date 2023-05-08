d76c2e7123c4574e8f0298da7390b4b7fcc08d14
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-08 22:44:50 +0700

    dong goi

---
+++ src/OOP/Test.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class Test {
+    public static void  main(String[] args) {
+        donggoi s = new donggoi();
+        s.setName("Manh");
+        System.out.println(s.getName());
+    }
+}
