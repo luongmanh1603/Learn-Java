cf09deddba52af4efc96fa44abb87a23ae68663e
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-08 22:44:48 +0700

    dong goi

---
+++ src/OOP/donggoi.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class donggoi {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
