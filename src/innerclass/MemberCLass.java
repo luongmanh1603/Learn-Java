c442e9ced43535f7075b4dca81f41c0278949b49
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-05-16 11:13:01 +0700

    dong goi

---
+++ src/innerclass/MemberCLass.java
@@ -0,0 +1,24 @@
+package innerclass;
+
+public class MemberCLass {
+
+    public class InnerMenberClas {
+        private int id;
+
+        public void In(){
+            System.out.println("Hello");
+        }
+    }
+    // chỉ có thể thừa kế ngay trong lớp chua nó
+
+    public class InnerMemberClass2 extends InnerMenberClas {
+
+    }
+
+    public static void main(String[] args) {
+        MemberCLass m = new MemberCLass();
+        MemberCLass.InnerMenberClas in = m.new InnerMenberClas();
+
+        in.In();
+    }
+}
