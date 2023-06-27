4703b05acee67709ceec2a85e36eecec5ad3fe7e
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-06-27 09:17:36 +0700

    equals

---
+++ src/OOP/Equals.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class Equals {
+    public static void main(String[] args) {
+        String s1 = new String("Luong Van Manh");
+        String s2 = new String("Luong Van Manh");
+        System.out.println("s1 == s2: " + (s1 == s2));
+        System.out.println("s1 equals s2: "+ (s1.equals(s2)));
+    }
+}
