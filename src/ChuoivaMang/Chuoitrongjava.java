9a539c8b57e7999766d2f5bc3a0c84edfc877949
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-07-25 20:35:42 +0700

    khaibaochuoi

---
+++ src/ChuoivaMang/Chuoitrongjava.java
@@ -0,0 +1,14 @@
+package ChuoivaMang;
+
+public class Chuoitrongjava {
+
+
+    public static void main(String[] args) {
+        String s1 = new String("manh");
+        System.out.println(s1);
+
+
+        String nameP = "quan";
+        System.out.println(nameP);
+    }
+}
