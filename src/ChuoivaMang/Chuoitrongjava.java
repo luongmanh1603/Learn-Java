7248f42fa41d6a849d4d75e4759db4dbee9b00aa
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-07-25 21:12:46 +0700

    cac ham ve chuoi

--- src/ChuoivaMang/Chuoitrongjava.java
+++ src/ChuoivaMang/Chuoitrongjava.java
@@ -14,14 +14,14 @@ public class Chuoitrongjava {
 
         String nameP = "quan";
         System.out.println(nameP);
-        //do dai chuoi
-        String address;
-        int dodai;
-        System.out.println("Dia chi: ");
-        address = sc.nextLine();
-
-        dodai = address.length();
-        System.out.println(dodai);
+//        //do dai chuoi
+//        String address;
+//        int dodai;
+//        System.out.println("Dia chi: ");
+//        address = sc.nextLine();
+//
+//        dodai = address.length();
+//        System.out.println(dodai);
 
         //noi 2 chuoi ki tu
         String s3;
@@ -33,7 +33,27 @@ public class Chuoitrongjava {
         s4 = sc.nextLine();
         String s5 = s3.concat(s4);
         System.out.println("Thanh chuoi:"+ s5);
+        //tra ve ky tu trong chuoi
+        char c = s5.charAt(2);
+        System.out.println(c);
+        // so sanh 2 chuoi
+        int sosanh = s3.compareTo(s4);
+        if (sosanh == 0){
+            System.out.println("2 so bang nhau");
 
+        } else {
+            System.out.println("2 so khac nhau");
+
+        }
+        //ham tra ve vi tri xuat hien
+        int re = s5.indexOf(s4);
+        int re1 = s5.indexOf(s3);
+        System.out.println(re);
+        System.out.println(re1);
+        System.out.println(s5.replace(s4,s3));
+
+        String s6 = s5.substring(5);
+        System.out.println(s6);
 
     }
 }
