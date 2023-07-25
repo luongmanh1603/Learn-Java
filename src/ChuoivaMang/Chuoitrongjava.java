5da7d8de40ae4c356dedb09f1e58784fcf7dc797
luongmanh1604 <114908089+luongmanh1604@users.noreply.github.com>
2023-07-25 20:46:45 +0700

    concat() noichuoi

--- src/ChuoivaMang/Chuoitrongjava.java
+++ src/ChuoivaMang/Chuoitrongjava.java
@@ -6,6 +6,8 @@ public class Chuoitrongjava {
 
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         String s1 = new String("manh");
         System.out.println(s1);
 
@@ -15,12 +17,22 @@ public class Chuoitrongjava {
         //do dai chuoi
         String address;
         int dodai;
-        Scanner sc = new Scanner(System.in);
         System.out.println("Dia chi: ");
         address = sc.nextLine();
 
         dodai = address.length();
         System.out.println(dodai);
+
+        //noi 2 chuoi ki tu
+        String s3;
+        String s4;
+
+        System.out.println("String3:");
+        s3 = sc.nextLine();
+        System.out.println("String4:");
+        s4 = sc.nextLine();
+        String s5 = s3.concat(s4);
+        System.out.println("Thanh chuoi:"+ s5);
 
 
     }
